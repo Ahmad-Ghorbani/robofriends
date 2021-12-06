47c8c449d6cfccf97daa1ba9176ae6487fe36837
Ahmad Ghorbani <56560408+Ahmad-Ghorbani@users.noreply.github.com>
2021-12-06 16:28:17 +0330

    Redux- update the app with redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,28 +1,45 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import CardList from "../components/CardList";
 import Scroll from "../components/Scroll";
 import SearchBox from "../components/SearchBox";
 import "./App.css";
+import { connect } from "react-redux";
 
-function App() {
-  const [robots, setRobots] = useState([]);
-  const [searchfield, setSearchfield] = useState("");
+import { setSearchField, requestRobots } from "../actions";
+
+const mapStateToProps = (state) => {
+  return {
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    SearchedRobots: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots()),
+  };
+};
+
+function App(store) {
+  //const [robots, setRobots] = useState([]);
+  //const [searchfield, setSearchfield] = useState("");
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((users) => setRobots(users));
-  }, [searchfield]);
+    store.onRequestRobots();
 
-  const SearchedRobots = (event) => {
-    setSearchfield(event.target.value);
-  };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const { searchField, SearchedRobots, robots, isPending } = store;
 
   const filteredRobots = robots.filter((robots) =>
-    robots.name.toLowerCase().includes(searchfield.toLowerCase())
+    robots.name.toLowerCase().includes(searchField.toLowerCase())
   );
 
-  return !robots.length ? (
+  return isPending ? (
     <div className="tc">
       <h1 className="f1">Loading</h1>
     </div>
@@ -37,4 +54,4 @@ function App() {
   );
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
